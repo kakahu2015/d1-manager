4cb3b7dacaf4da7620aa25c4d1e1c3089612e5fc
kakahu <kakahu2021@gmail.com>
2024-07-01 09:47:20 +0800

    Update index.ts
    ai-sql

--- src/lib/server/ai/index.ts
+++ src/lib/server/ai/index.ts
@@ -1,3 +1,4 @@
+import { env } from "$env/dynamic/private";
 import type { BaseChatMessage, BaseChatMessageRole, BaseChatParam } from "@ai-d/aid";
 import { Aid } from "@ai-d/aid";
 import debug from "debug";
@@ -7,37 +8,66 @@ debug.enable("aid*");
 const log = debug("assistant");
 log.enabled = true;
 
-const CFAI_MODEL = "@cf/defog/sqlcoder-7b-2";
+const OPENAI_MODEL = env.OPENAI_MODEL || "gpt-3.5-turbo-1106";
+const CFAI_MODEL = env.CFAI_MODEL || "@cf/defog/sqlcoder-7b-2";
 
 /**
- * Returns the AI model information.
+ * Checks the availability of AI models.
  */
-export function getAiInfo(): string {
+export function available(): string | null {
+	//if (env.OPENAI_API_KEY) {
+	//	return `OpenAI (${OPENAI_MODEL})`;
+	//}
+
+	//if (env.AI) {
+	//	return `Cloudflare (${CFAI_MODEL})`;
+	//}
+
+	//return null;
     return `Cloudflare (${CFAI_MODEL})`;
 }
 
-export type AiBackend = Aid<
-    string,
-    BaseChatParam,
-    BaseChatMessage<BaseChatMessageRole>[],
-    BaseChatMessage<BaseChatMessageRole>[]
->;
+export type AiBackend =
+	| Aid<
+			string,
+			BaseChatParam,
+			BaseChatMessage<BaseChatMessageRole>[],
+			BaseChatMessage<BaseChatMessageRole>[]
+	  >
+	| undefined;
 
 export async function select_backend(): Promise<AiBackend> {
-    log("using Cloudflare backend");
-    return Aid.chat(async (messages) => {
-        const { Ai } = await import("$lib/../../cfai/cfai");
-        const ai = new Ai(); // 假设 Ai 构造函数不需要参数
-        const { response } = await ai.run(CFAI_MODEL, {
-            messages,
-        });
+	if (1>2) {
+		log("using OpenAI backend");
+		const { OpenAI } = await import("openai");
+		return Aid.from(
+			new OpenAI({
+				baseURL: env.OPENAI_API_URL,
+				apiKey: env.OPENAI_API_KEY,
+			}),
+			{ model: OPENAI_MODEL },
+		);
+	}
 
-        const res: string = response;
-        const match = res.match(/{\s*"sql":\s*".*?"\s*}/);
-        if (match) {
-            return match[0];
-        }
+	if (1<2) {
+		log("using Cloudflare backend");
+		return Aid.chat(async (messages) => {
+			const { Ai } = await import("$lib/../../cfai/cfai");
+			const ai = new Ai(env.AI);
+			const { response } = await ai.run(CFAI_MODEL, {
+				messages,
+			});
 
-        return res;
-    });
+			const res: string = response;
+			const match = res.match(/{\s*"sql":\s*".*?"\s*}/);
+			if (match) {
+				return match[0];
+			}
+
+			return res;
+		});
+	}
+
+	log("no backend");
+	return undefined;
 }
